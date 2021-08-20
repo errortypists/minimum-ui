3dddfda9bf63ba2c094685f6cfc439daa727f246
Yeongtae Im <ytim@zigbang.com>
2021-08-21 01:22:39 +0900

    yarn workspace 샘플

---
+++ packages/ui-kit/rollup.config.js
@@ -0,0 +1,27 @@
+import resolve from "@rollup/plugin-node-resolve";
+import image from "@rollup/plugin-image";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      dir: "build",
+      format: "esm",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  preserveModules: true,
+  plugins: [
+    peerDepsExternal(),
+    image(),
+    resolve(),
+    commonjs({
+      include: /node_modules/,
+    }),
+    typescript({ useTsconfigDeclarationDir: true }),
+  ],
+};
