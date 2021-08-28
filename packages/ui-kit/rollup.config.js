e2a57ec32576b7f874cd25bfb594f1be485e5ba8
ytim <skyimt33@gmail.com>
2021-08-28 14:06:10 +0900

    babel 설정 변경

--- packages/ui-kit/rollup.config.js
+++ packages/ui-kit/rollup.config.js
@@ -5,39 +5,24 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-const packageJson = require("./package.json");
+const pkg = require("./package.json");
 const external = ["react", "react-dom", "styled-components"];
 
 export default {
   input: "./src/index.ts",
-  // output: [
-  //   {
-  //     dir: "dist",
-  //     format: "esm",
-  //     exports: "named",
-  //     sourcemap: true,
-  //   },
-  // ],
   output: [
     {
-      dir: "dist/esm",
-      format: "es",
-      preserveModules: true,
-      // plugins: [
-      //   getBabelOutputPlugin({
-      //     plugins: ["babel-plugin-styled-components"],
-      //   }),
-      // ],
+      file:pkg.main,
+      format: "esm",
+      sourcemap:true
     },
     {
-      dir: "dist/cjs",
+      file:pkg.module,
       format: "cjs",
-      preserveModules: true,
-      exports: "auto",
+      sourcemap:true
     },
   ],
   external,
-  preserveModules: true,
   plugins: [
     peerDepsExternal(),
     image(),
