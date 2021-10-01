0a6bbd25c1e5b82109ac9e84449db8e04d123fd5
Yeongtae Im <ytim@zigbang.com>
2021-10-01 22:27:39 +0900

    컴포넌트 임시 doc 사이트에 표현

--- packages/ui-kit-docs/src/pages/Tabs/index.tsx
+++ packages/ui-kit-docs/src/pages/Tabs/index.tsx
@@ -1,9 +1,48 @@
-import React from "react";
-import styled from "styled-components";
-// import { Button2, Tabs } from "ui-kit";
+import React from "react"
+import { Tabs, IconArrowRight } from "ui-kit"
 
-const Tabs: React.FC = () => {
-  return <div>122345</div>;
-};
+const TabsPage: React.FC = () => {
+  return (
+    <>
+      <Tabs
+        headers={[
+          { text: "탭1", icon: <IconArrowRight /> },
+          { text: "탭2" },
+          { text: "탭3" },
+        ]}
+        hasRadius
+        onChange={(item, index) => {
+          console.log(item, index)
+        }}
+      />
+      <hr />
+      <Tabs
+        headers={[
+          { text: "탭1" },
+          { text: "탭2" },
+          { text: "탭3", icon: <IconArrowRight /> },
+          { text: "탭4", icon: <IconArrowRight /> },
+        ]}
+        hasRadius
+        onChange={(item, index) => {
+          console.log(item, index)
+        }}
+      />
+      <hr />
+      <Tabs
+        headers={[
+          { text: "탭1" },
+          { text: "탭2" },
+          { text: "탭3" },
+          { text: "탭4" },
+          { text: "탭5" },
+        ]}
+        onChange={(item, index) => {
+          console.log(item, index)
+        }}
+      />
+    </>
+  )
+}
 
-export default Tabs;
+export default TabsPage
