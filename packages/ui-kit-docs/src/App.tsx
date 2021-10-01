50a415feeeddeabe86acab110643930ac85307e8
Yeongtae Im <ytim@zigbang.com>
2021-10-01 23:49:51 +0900

    프로그레스 doc 연결

--- packages/ui-kit-docs/src/App.tsx
+++ packages/ui-kit-docs/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
-import { Tabs } from "./pages";
+import { Tabs, Progress } from "./pages"
 
 function App() {
   return (
@@ -10,14 +10,18 @@ function App() {
         <Route path="/" exact>
           <div>
             <Link to="/">Home</Link> /<Link to="/tabs">tabs</Link> /
+            <Link to="/progress">progress</Link> /
           </div>
         </Route>
         <Route path="/tabs" exact>
           <Tabs />
         </Route>
+        <Route path="/progress" exact>
+          <Progress />
+        </Route>
       </Switch>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
