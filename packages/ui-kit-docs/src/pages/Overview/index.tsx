bff619544663534120d393a3484ad399555a157c
kdilot <kdilotdev@gmail.com>
2022-01-30 20:47:20 +0900

    Modify left wrapper

--- packages/ui-kit-docs/src/pages/Overview/index.tsx
+++ packages/ui-kit-docs/src/pages/Overview/index.tsx
@@ -4,7 +4,7 @@ import Header from "../../components/Header"
 const OverviewPage: React.FC = () => {
   return (
     <Layout.MainLayout>
-      <Layout.LeftWrapper>123</Layout.LeftWrapper>
+      <Layout.LeftWrapper />
       <Layout.RightWrapper>
         <Header></Header>
       </Layout.RightWrapper>
