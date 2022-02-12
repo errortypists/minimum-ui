c48f9e724e13f65a97c45deecde732977d85381a
kdilot <kdilotdev@gmail.com>
2022-02-12 20:54:39 +0900

    Add divider, typography layout

--- packages/ui-kit-docs/src/pages/Layout/index.tsx
+++ packages/ui-kit-docs/src/pages/Layout/index.tsx
@@ -1,38 +1,74 @@
 import styled from "styled-components"
-import { Divider, Flex, Grid } from "ui-kit"
+import { Flex, Grid } from "ui-kit"
+import Layout from "../../components/Layout"
+
+const {
+  ContentsLayout,
+  ComponentWrapper,
+  CodeWrapper,
+  Title,
+  Description,
+  ComponentTitle,
+} = Layout
 
 const LayoutPage: React.FC = () => {
   return (
-    <Wrapper>
-      <Flex>
-        <Box style={{ flex: 1 }}>FLEX1</Box>
-        <Box style={{ flex: 2 }}>FLEX2</Box>
-        <Box style={{ flex: 3 }}>FLEX3</Box>
-        <Box style={{ flex: 2 }}>FLEX4</Box>
-        <Box style={{ flex: 1 }}>FLEX5</Box>
-      </Flex>
-      <Divider size={20} />
-      <Flex direction="column" style={{ height: "300px" }}>
-        <Box style={{ flex: 2, height: "auto" }}>FLEX1</Box>
-        <Box style={{ flex: 1, height: "auto" }}>FLEX2</Box>
-        <Box style={{ flex: 3, height: "auto" }}>FLEX3</Box>
-        <Box style={{ flex: 1, height: "auto" }}>FLEX4</Box>
-        <Box style={{ flex: 2, height: "auto" }}>FLEX5</Box>
-      </Flex>
-      <Divider size={20} />
-      <Grid row={2} col={2} gap={8}>
-        <Box>GRID1</Box>
-        <Box>GRID2</Box>
-        <Box>GRID3</Box>
-        <Box>GRID4</Box>
-      </Grid>
-    </Wrapper>
+    <ContentsLayout>
+      <Title>Layout</Title>
+      <Description>Flex, Grid를 활용한 레이아웃 구성</Description>
+      <ComponentTitle>Flex</ComponentTitle>
+      <ComponentWrapper>
+        <Flex>
+          <Box style={{ flex: 1 }}>FLEX1</Box>
+          <Box style={{ flex: 2 }}>FLEX2</Box>
+          <Box style={{ flex: 1 }}>FLEX3</Box>
+        </Flex>
+      </ComponentWrapper>
+      <CodeWrapper
+        code={`<Flex>
+          <Box style={{ flex: 1 }}>FLEX1</Box>
+          <Box style={{ flex: 2 }}>FLEX2</Box>
+          <Box style={{ flex: 1 }}>FLEX3</Box>
+</Flex>`}
+      />
+      <ComponentWrapper>
+        <Flex direction="column" style={{ height: "300px" }}>
+          <Box style={{ flex: 2, height: "auto" }}>FLEX1</Box>
+          <Box style={{ flex: 1, height: "auto" }}>FLEX2</Box>
+          <Box style={{ flex: 3, height: "auto" }}>FLEX3</Box>
+          <Box style={{ flex: 1, height: "auto" }}>FLEX4</Box>
+          <Box style={{ flex: 2, height: "auto" }}>FLEX5</Box>
+        </Flex>
+      </ComponentWrapper>
+      <CodeWrapper
+        code={`<Flex direction="column" style={{ height: "300px" }}>
+          <Box style={{ flex: 2, height: "auto" }}>FLEX1</Box>
+          <Box style={{ flex: 1, height: "auto" }}>FLEX2</Box>
+          <Box style={{ flex: 3, height: "auto" }}>FLEX3</Box>
+          <Box style={{ flex: 1, height: "auto" }}>FLEX4</Box>
+          <Box style={{ flex: 2, height: "auto" }}>FLEX5</Box>
+</Flex>`}
+      />
+      <ComponentTitle>Grid</ComponentTitle>
+      <ComponentWrapper>
+        <Grid row={2} col={2} gap={8}>
+          <Box>GRID1</Box>
+          <Box>GRID2</Box>
+          <Box>GRID3</Box>
+          <Box>GRID4</Box>
+        </Grid>
+      </ComponentWrapper>
+      <CodeWrapper
+        code={`<Grid row={2} col={2} gap={8}>
+          <Box>GRID1</Box>
+          <Box>GRID2</Box>
+          <Box>GRID3</Box>
+          <Box>GRID4</Box>
+</Grid>`}
+      />
+    </ContentsLayout>
   )
 }
-
-const Wrapper = styled.div`
-  width: 500px;
-`
 
 const Box = styled.div`
   width: 100%;
--- packages/ui-kit-docs/src/pages/Typography/index.tsx
+++ packages/ui-kit-docs/src/pages/Typography/index.tsx
@@ -1,36 +1,56 @@
 import styled from "styled-components"
 import { Typography } from "ui-kit"
+import Layout from "../../components/Layout"
 
 const { Title, SubTitle, Body, Caption } = Typography
+const {
+  ContentsLayout,
+  ComponentWrapper,
+  CodeWrapper,
+  Title: LayoutTitle,
+  Description,
+  ComponentTitle,
+  ComponentDescription,
+} = Layout
 
 const TypographyPage: React.FC = () => {
   return (
-    <Wrapper>
-      <Title>
-        안녕하세요.
-        <br />
-        제목으로 쓰이는 공간입니다
-      </Title>
-      <SubTitle>부제목으로 쓰이는 공간이에요</SubTitle>
-      <Body>
-        내용입니다 내용 내용입니다 내용 내용입니다 내용 내용입니다 내용
-        내용입니다 내용 내용입니다 내용 내용입니다 내용 내용입니다 내용
-        내용입니다 내용 내용입니다 내용 내용입니다 내용 내용입니다 내용
-        내용입니다 내용 내용입니다 내용
-      </Body>
-      <Body>
-        내용입니다 내용 내용입니다 내용 내용입니다 내용 내용입니다 내용
-        내용입니다 내용 내용입니다 내용 내용입니다 내용 내용입니다 내용
-        내용입니다 내용 내용입니다 내용 내용입니다 내용 내용입니다 내용
-        내용입니다 내용 내용입니다 내용
-      </Body>
-      <Caption>사이드 문구입니다</Caption>
-    </Wrapper>
+    <ContentsLayout>
+      <LayoutTitle>Typography</LayoutTitle>
+      <Description>
+        기본 텍스트 스타일 title, subtitle, body, caption
+      </Description>
+      <ComponentWrapper>
+        <Title>Title</Title>
+        <SubTitle>Subtitle</SubTitle>
+        <Body>This is Body</Body>
+        <Caption>This is Caption</Caption>
+      </ComponentWrapper>
+      <CodeWrapper
+        code={`const { Title, SubTitle, Body, Caption } = Typography
+        
+<Title>Title</Title>
+<SubTitle>Subtitle</SubTitle>
+<Body>This is Body</Body>
+<Caption>This is Caption</Caption>`}
+      />
+      <ComponentTitle>Style</ComponentTitle>
+      <ComponentWrapper>
+        <Title style={{ color: "red" }}>Title</Title>
+        <SubTitle style={{ textDecoration: "line-through" }}>Subtitle</SubTitle>
+        <Body style={{ padding: "10px" }}>This is Body</Body>
+        <Caption style={{ letterSpacing: 3 }}>This is Caption</Caption>
+      </ComponentWrapper>
+      <CodeWrapper
+        code={`const { Title, SubTitle, Body, Caption } = Typography
+
+<Title style={{ color: "red" }}>Title</Title>
+<SubTitle style={{ textDecoration: "line-through" }}>Subtitle</SubTitle>
+<Body style={{ padding: "10px" }}>This is Body</Body>
+<Caption style={{ letterSpacing: 3 }}>This is Caption</Caption>`}
+      />
+    </ContentsLayout>
   )
 }
-
-const Wrapper = styled.div`
-  width: 500px;
-`
 
 export default TypographyPage
