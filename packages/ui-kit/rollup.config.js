6c57af6048e310faefd1101cc556b00e01e315f9
Yeongtae Im <ytim@zigbang.com>
2022-04-02 19:48:12 +0900

    롤업 어글리파이 추가, npm설명문 추가

--- packages/ui-kit/rollup.config.js
+++ packages/ui-kit/rollup.config.js
@@ -1,14 +1,15 @@
-import resolve from "@rollup/plugin-node-resolve";
-import image from "@rollup/plugin-image";
-import babel, { getBabelOutputPlugin } from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import svgr from "@svgr/rollup"; //  SVG를 컴포넌트 형태로 불러와서 사용
-import url from "@rollup/plugin-url"; //  데이터 URI 또는 ES모듈로 가져옴 (@svgr/rollup 사용시 필요)
+import resolve from "@rollup/plugin-node-resolve"
+import image from "@rollup/plugin-image"
+import babel, { getBabelOutputPlugin } from "@rollup/plugin-babel"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "rollup-plugin-typescript2"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import svgr from "@svgr/rollup" //  SVG를 컴포넌트 형태로 불러와서 사용
+import url from "@rollup/plugin-url" //  데이터 URI 또는 ES모듈로 가져옴 (@svgr/rollup 사용시 필요)
+import { uglify } from "rollup-plugin-uglify"
 
-const pkg = require("./package.json");
-const external = ["react", "react-dom", "styled-components"];
+const pkg = require("./package.json")
+const external = ["react", "react-dom", "styled-components"]
 
 export default {
   input: "./src/index.ts",
@@ -38,5 +39,6 @@ export default {
     }),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
+    uglify(),
   ],
-};
+}
